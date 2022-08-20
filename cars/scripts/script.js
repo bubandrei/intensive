39a6ea738d799a736b04d07adcaf4a0fb4787328
a.bubelev <bubeleu@gmail.com>
2022-08-20 21:53:55 +0300

    button ready

--- cars/scripts/script.js
+++ cars/scripts/script.js
@@ -7,4 +7,16 @@ for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function () {
         document.getElementById("price").scrollIntoView({behavior: "smooth"});
     }
-;}
+};
+
+document.getElementById("price-action").onclick = function (){
+    if(document.getElementById("name").value === ''){
+        alert("Заполните поле имя!")
+    } else if(document.getElementById("phone").value === ''){
+        alert("Заполните поле телефон!")
+    } else if(document.getElementById("car").value === ''){
+        alert("Заполните поле автомобиль!")
+    } else{
+    alert("Спасибо за заявку. Мы свяжемся с вами в ближайшее время!")}
+}
+
