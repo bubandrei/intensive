3bd892e2ad088f9aba556ee7c35206b8101507b1
a.bubelev <bubeleu@gmail.com>
2022-08-21 13:10:25 +0300

    listener for mouse

--- cars-app/src/app/app.component.ts
+++ cars-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
@@ -63,14 +63,32 @@ export class AppComponent {
   constructor(private fb: FormBuilder) {
   };
 
-  goScroll(target: HTMLElement) {
-    target.scrollIntoView({behavior: "smooth"})
+  goScroll(target: HTMLElement, car?: any) {
+    target.scrollIntoView({behavior: "smooth"});
+    if (car) {
+      this.priceForm.patchValue({car: car.name})
+    }
   };
+
+  trans: any;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+  }
+
+  bgPos: any;
+
+  @HostListener('document:scroll', ['$event'])
+  onScroll() {
+    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+  }
 
   onSubmit() {
     if (this.priceForm.valid) {
       alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!");
       this.priceForm.reset();
-    };
+    }
+    ;
   };
 }
