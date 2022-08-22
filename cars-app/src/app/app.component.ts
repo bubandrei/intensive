7203018d2091522af2fc22acc4ddb7a6ecf6aee9
a.bubelev <bubeleu@gmail.com>
2022-08-22 20:02:07 +0300

    response ready

--- cars-app/src/app/app.component.ts
+++ cars-app/src/app/app.component.ts
@@ -16,53 +16,59 @@ export class AppComponent {
     car: ['', Validators.required]
   });
 
-  carsData = [
-    {
-      image: "1.png",
-      name: "Lamborghini Huracan Spyder",
-      gear: "полный",
-      engine: 5.2,
-      places: 2
-    },
-    {
-      image: "2.png",
-      name: "Chevrolet Corvette",
-      gear: "полный",
-      engine: 6.2,
-      places: 2
-    },
-    {
-      image: "3.png",
-      name: "Ferrari California",
-      gear: "полный",
-      engine: 3.9,
-      places: 2
-    },
-    {
-      image: "4.png",
-      name: "Lamborghini Urus",
-      gear: "полный",
-      engine: 4.0,
-      places: 5
-    },
-    {
-      image: "5.png",
-      name: "Audi R8",
-      gear: "полный",
-      engine: 5.2,
-      places: 2
-    },
-    {
-      image: "6.png",
-      name: "Chevrolet Camaro",
-      gear: "полный",
-      engine: 2.0,
-      places: 4
-    }
-  ];
+  carsData: any;
+
+  // carsData = [
+  //   {
+  //     image: "1.png",
+  //     name: "Lamborghini Huracan Spyder",
+  //     gear: "полный",
+  //     engine: 5.2,
+  //     places: 2
+  //   },
+  //   {
+  //     image: "2.png",
+  //     name: "Chevrolet Corvette",
+  //     gear: "полный",
+  //     engine: 6.2,
+  //     places: 2
+  //   },
+  //   {
+  //     image: "3.png",
+  //     name: "Ferrari California",
+  //     gear: "полный",
+  //     engine: 3.9,
+  //     places: 2
+  //   },
+  //   {
+  //     image: "4.png",
+  //     name: "Lamborghini Urus",
+  //     gear: "полный",
+  //     engine: 4.0,
+  //     places: 5
+  //   },
+  //   {
+  //     image: "5.png",
+  //     name: "Audi R8",
+  //     gear: "полный",
+  //     engine: 5.2,
+  //     places: 2
+  //   },
+  //   {
+  //     image: "6.png",
+  //     name: "Chevrolet Camaro",
+  //     gear: "полный",
+  //     engine: 2.0,
+  //     places: 4
+  //   }
+  // ];
 
   constructor(private fb: FormBuilder, private appService: AppService) {
   };
+
+  ngOnInit() {
+    this.appService.getData().subscribe(carsData => this.carsData = carsData);
+  }
 
   goScroll(target: HTMLElement, car?: any) {
     target.scrollIntoView({behavior: "smooth"});
@@ -90,17 +96,18 @@ export class AppComponent {
 
       this.appService.sendQuery(this.priceForm.value)
         .subscribe({
-          next:(response:any)=>{
+          next: (response: any) => {
             alert(response.message);
             this.priceForm.reset();
           },
-          error:(response)=>{
+          error: (response) => {
             alert(response.error.message);
           }
         });
 
       alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!");
       this.priceForm.reset();
-    };
+    }
+    ;
   };
 }
